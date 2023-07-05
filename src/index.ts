2ca7a8cb0cd57aae66ddfdca7ce1547a8c088157
saspadani <aspadani@gmail.com>
2023-07-05 21:19:59 +0700

    modif

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Application = express();
 const port: Number = 4000;
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: 200 });
+  res.status(400).send({ status: 200 });
 });
 
 app.listen(port, () => console.log(`server is listening from ${port}`));
