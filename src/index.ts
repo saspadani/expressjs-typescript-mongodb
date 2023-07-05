a27cbc07e6c948abed22f80d6df6e89c128daf06
saspadani <aspadani@gmail.com>
2023-07-05 21:54:23 +0700

    index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 const port: Number = 4000;
 
-app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-  res.status(400).send({ status: 200 });
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: 200 });
 });
 
 app.listen(port, () => console.log(`server is listening from ${port}`));
