4a1d57d51f5d76f15fcac659f085f4e513a301b2
saspadani <aspadani@gmail.com>
2023-07-07 06:13:00 +0700

    fix typo in pre-commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { type Application, type Request, type Response, type NextFunction } from 'express'
 
-const app: Application = express();
-const port: Number = 4000;
+const app: Application = express()
+const port: number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: 200 });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: 200 })
+})
 
-app.listen(port, () => console.log(`server is listening from ${port}`));
+app.listen(port, () => {
+  console.log(`server is listening from ${port}`)
+})
