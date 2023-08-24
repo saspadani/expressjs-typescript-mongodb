72b18828e6cbbc1e2146a3aa9d570fa127dec8b2
saspadani <aspadani@gmail.com>
2023-08-24 22:17:32 +0700

    health route

---
+++ src/routes/health.ts
@@ -0,0 +1,7 @@
+import { type Request, type Response, type NextFunction, Router } from 'express'
+
+export const HealthRouter: Router = Router()
+
+HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: '200' })
+})
