fcc65873ed0ef28c4dd00edb3855cf96c046e9b7
saspadani <aspadani@gmail.com>
2023-08-24 22:17:02 +0700

    app.use new route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express, { type Application, type Request, type Response, type NextFunction } from 'express'
+import express, { type Application } from 'express'
+import { routes } from './routes'
 
 const app: Application = express()
 const port: number = 4000
 
-app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: 200 })
-})
+routes(app)
 
 app.listen(port, () => {
   console.log(`server is listening from ${port}`)
