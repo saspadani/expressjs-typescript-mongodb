2845ac87e3f30146e1834114b964be9025b2936f
saspadani <aspadani@gmail.com>
2023-08-29 23:28:23 +0700

    controller

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,44 @@
+import { type Request, type Response } from 'express'
+import { createProductValidation } from '../validations/product.validation'
+import { logger } from '../utils/logger'
+
+export const createProduct = (req: Request, res: Response) => {
+  const { error, value } = createProductValidation(req.body)
+
+  if (error) {
+    logger.error('ERR: product - create = ', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+  }
+
+  logger.info('post new product data success')
+  res.status(200).send({ status: true, statusCode: 200, message: 'add product success', data: value })
+}
+
+export const getProduct = (req: Request, res: Response) => {
+  const products = [
+    { name: 'sepatu', price: 200000 },
+    { name: 'kaos', price: 100000 }
+  ]
+  const {
+    params: { name }
+  } = req
+
+  if (name) {
+    const filterProduct = products.filter((product) => {
+      if (product.name === name) {
+        return product
+      }
+    })
+
+    if (filterProduct.length === 0) {
+      logger.info('Data not found')
+      return res.status(404).send({ status: false, statusCode: 404, data: {} })
+    }
+
+    logger.info('get product params data success')
+    return res.status(200).send({ status: true, statusCode: 200, data: filterProduct[0] })
+  }
+
+  logger.info('get product data success')
+  res.status(200).send({ status: true, statusCode: 200, data: products })
+}
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,22 +1,8 @@
-import { type Request, type Response, type NextFunction, Router } from 'express'
-import { logger } from '../utils/logger'
-import { createProductValidation } from '../validation/product.validation'
+import { Router } from 'express'
+import { createProduct, getProduct } from '../controllers/product.controller'
 
 export const ProductRouter: Router = Router()
 
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  logger.info('get product data success')
-  res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 500000 }] })
-})
-
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = createProductValidation(req.body)
-
-  if (error) {
-    logger.error('ERR: product - create = ', error.details[0].message)
-    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
-  }
-
-  logger.info('post new product data success')
-  res.status(200).send({ status: true, statusCode: 200, message: 'add product success', data: value })
-})
+ProductRouter.get('/', getProduct)
+ProductRouter.get('/:name', getProduct)
+ProductRouter.post('/', createProduct)
