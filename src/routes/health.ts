3688ffd5719e3c6d575811ee8af445c5fd4331d3
saspadani <aspadani@gmail.com>
2023-08-29 18:31:04 +0700

    add logger

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,9 @@
 import { type Request, type Response, type NextFunction, Router } from 'express'
+import { logger } from '../utils/logger'
 
 export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Health check success')
   res.status(200).send({ status: '200' })
 })
