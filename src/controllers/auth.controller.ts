ef845a9e345e81946a4ef91d3a95615f71e64802
saspadani <aspadani@gmail.com>
2023-09-03 21:16:35 +0700

    create createSession

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import { type Request, type Response } from 'express'
-import { createUserValidation } from '../validations/auth.validation'
+import { createSessionValidation, createUserValidation } from '../validations/auth.validation'
 import { v4 as uuidv4 } from 'uuid'
 import { logger } from '../utils/logger'
-import { hashing } from '../utils/hashing'
-import { createUser } from '../services/auth.service'
+import { checkPassword, hashing } from '../utils/hashing'
+import { createUser, findUserByEmail } from '../services/auth.service'
+// import type UserType from '../types/user.type'
+import { signJWT } from '../utils/jwt'
 
 export const registerUser = async (req: Request, res: Response) => {
   req.body.user_id = uuidv4()
@@ -22,5 +24,28 @@ export const registerUser = async (req: Request, res: Response) => {
   } catch (error) {
     logger.error('ERR: auth - register = ', error)
     return res.status(422).send({ status: false, statusCode: 422, message: error })
+  }
+}
+
+export const createSession = async (req: Request, res: Response) => {
+  const { error, value } = createSessionValidation(req.body)
+
+  if (error) {
+    logger.error('ERR: auth - create session = ', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message })
+  }
+
+  try {
+    const user: any = await findUserByEmail(value.email)
+    const isValid = checkPassword(value.password, user.password)
+
+    if (!isValid) return res.status(401).json({ status: false, statusCode: 401, message: 'Invalid Email or Password' })
+
+    const accessToken = signJWT({ ...user }, { expiresIn: '1d' })
+
+    return res.status(200).send({ status: true, statusCode: 200, message: 'Login success', data: { accessToken } })
+  } catch (error: any) {
+    logger.error(`ERR: auth - create session = ${error.message}`)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.message })
   }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/auth.controller'
+import { registerUser, createSession } from '../controllers/auth.controller'
 
 export const AuthRouter: Router = Router()
 
 AuthRouter.post('/register', registerUser)
-// AuthRouter.get('/:id', getAuth)
-// AuthRouter.post('/', createAuth)
-// AuthRouter.put('/:id', updateAuth)
-// AuthRouter.delete('/:id', deleteAuth)
+AuthRouter.post('/login', createSession)
