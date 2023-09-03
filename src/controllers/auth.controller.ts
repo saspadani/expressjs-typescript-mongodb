a0ed3694a38e98e351cf87414d04be28ac4cb8cf
saspadani <aspadani@gmail.com>
2023-09-03 23:50:01 +0700

    add refresh token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,11 @@
 import { type Request, type Response } from 'express'
-import { createSessionValidation, createUserValidation } from '../validations/auth.validation'
+import { createSessionValidation, createUserValidation, refreshSessionValidation } from '../validations/auth.validation'
 import { v4 as uuidv4 } from 'uuid'
 import { logger } from '../utils/logger'
 import { checkPassword, hashing } from '../utils/hashing'
 import { createUser, findUserByEmail } from '../services/auth.service'
 // import type UserType from '../types/user.type'
-import { signJWT } from '../utils/jwt'
+import { signJWT, verifyJWT } from '../utils/jwt'
 
 export const registerUser = async (req: Request, res: Response) => {
   req.body.user_id = uuidv4()
@@ -43,9 +43,38 @@ export const createSession = async (req: Request, res: Response) => {
 
     const accessToken = signJWT({ ...user }, { expiresIn: '1d' })
 
-    return res.status(200).send({ status: true, statusCode: 200, message: 'Login success', data: { accessToken } })
+    const refreshToken = signJWT({ ...user }, { expiresIn: '1y' })
+
+    return res
+      .status(200)
+      .send({ status: true, statusCode: 200, message: 'Login success', data: { accessToken, refreshToken } })
   } catch (error: any) {
     logger.error(`ERR: auth - create session = ${error.message}`)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.message })
+  }
+}
+
+export const refreshSession = async (req: Request, res: Response) => {
+  const { error, value } = refreshSessionValidation(req.body)
+
+  if (error) {
+    logger.error('ERR: auth - refresh session = ', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message })
+  }
+
+  try {
+    const { decoded }: any = verifyJWT(value.refreshToken)
+
+    const user = await findUserByEmail(decoded._doc.email)
+    if (!user) return false
+
+    const accessToken = signJWT({ ...user }, { expiresIn: '1d' })
+
+    return res
+      .status(200)
+      .send({ status: true, statusCode: 200, message: 'Refresh Session success', data: { accessToken } })
+  } catch (error: any) {
+    logger.error(`ERR: auth - refresh session = ${error.message}`)
     return res.status(422).send({ status: false, statusCode: 422, message: error.message })
   }
 }
